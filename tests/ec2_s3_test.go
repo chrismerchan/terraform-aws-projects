f3513d1ae43b8e6e427e88212b5f1fbef2003319
chrismerchan <christian.merchan@gmail.com>
2021-11-12 14:26:11 -0500

    Solve bug

--- tests/ec2_s3_test.go
+++ tests/ec2_s3_test.go
@@ -3,7 +3,6 @@ package tests
 import (
 	"testing"
   
-	"github.com/gruntwork-io/terratest/modules/aws"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
   )
