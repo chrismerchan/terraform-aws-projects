a095798102d2f025fb0b847df16f59218495a271
chrismerchan <christian.merchan@gmail.com>
2021-11-12 11:18:01 -0500

    Initial commit

---
+++ main/main.tf
@@ -0,0 +1,51 @@
+/**************************************
+*
+* Terraform Project Main configuration
+*
+***************************************/
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+
+# Create a bucket AWS S3
+resource "aws_s3_bucket" "s3_example" {
+  bucket = "bucket-s3-cmerchan"
+  acl    = "private" # or can be "public-read"
+  tags = {
+    Name        = "Flugel"
+    Environment = "Dev"
+    Owner       = "InfraTeam"
+  }
+  versioning {
+    enabled = true
+  }
+}
+
+#  Create a AWS EC2 Instance
+resource "aws_instance" "ec2_example" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+  tags = {
+    Name        = "Flugel"
+    Environment = "Dev"
+    Owner       = "InfraTeam"
+  }
+}
+
+# variables for automation testing
+output "bucket_id" {
+  value = aws_s3_bucket.s3_example.id
+}
+output "instance_id" {
+  value = aws_instance.ec2_example.tags.Name
+}
